5cdb2d71c3569f50c76bc4a8e3e62b2620fdeac8
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-26 15:00:00 -0300

    Add files via upload

---
+++ 00-setup.R
@@ -0,0 +1,26 @@
+packages <- c("bayesplot", 
+              "brms", 
+              "broom", 
+              "devtools", 
+              "flextable", 
+              "GGally", 
+              "ggmcmc", 
+              "ggrepel", 
+              "gtools", 
+              "loo", 
+              "patchwork", 
+              "psych", 
+              "Rcpp", 
+              "remotes", 
+              "rstan", 
+              "StanHeaders", 
+              "survival", 
+              "tidybayes", 
+              "tidyverse")
+
+install.packages(packages, dependencies = T)
+
+
+devtools::install_github("stan-dev/cmdstanr")
+remotes::install_github("stan-dev/posterior")
+devtools::install_github("rmcelreath/rethinking")
